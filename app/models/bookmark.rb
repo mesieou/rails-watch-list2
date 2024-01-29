42d6973e10f6e88e831beb4074c98c04fd5100d8
juan bernal <mesieou@gmail.com>
2024-01-29 11:10:14 +1100

    validations and models created

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+
+  validates_uniqueness_of :movie_id, scope: :list_id
+  validates :comment, presence: true, length: { minimum: 6 }
+end
