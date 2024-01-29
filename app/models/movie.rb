db22e155d80f2257e853363609dbeff35ae39b2b
juan bernal <mesieou@gmail.com>
2024-01-29 12:30:18 +1100

    seeds added with a movies api

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true, uniqueness: true
-
 end
