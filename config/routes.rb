9c3f683e931068a462fbfddd9d1f183b2aac48eb
juan bernal <mesieou@gmail.com>
2024-01-31 08:29:42 +1100

    reviews added in list show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :lists do
     resources :bookmarks, only: [:create, :index]
+    resources :reviews, only: [:create]
   end
   resources :bookmarks, only: [:destroy]
 end
